e85474a813495b20a296a79408578f990b9d0f17
Radu Margineanu <radu.margineanu@gmail.com>
2025-07-14 16:05:21 +0200

    Automatic fetch Github artifacts retry

---
+++ pkg/gretryclient/gretryclient.go
@@ -0,0 +1,17 @@
+package gretryclient
+
+import (
+  "net/http"
+
+	"github.com/darthbanana13/artifact-selector/pkg/log"
+
+	"github.com/hashicorp/go-retryablehttp"
+)
+
+func NewRetryClient(maxRetries int, logger log.ILogger) *http.Client {
+  retryClient := retryablehttp.NewClient()
+  retryClient.RetryMax = maxRetries
+  retryClient.Logger = NewLeveledLoggerAdapter(logger)
+
+  return retryClient.StandardClient()
+}
---
+++ pkg/gretryclient/loggeradapter.go
@@ -0,0 +1,35 @@
+package gretryclient
+
+import (
+	"github.com/darthbanana13/artifact-selector/pkg/log"
+)
+
+type LeveledLoggerAdapter struct {
+	log log.ILogger
+}
+
+func NewLeveledLoggerAdapter(logger log.ILogger) *LeveledLoggerAdapter {
+	return &LeveledLoggerAdapter{
+		log: logger,
+	}
+}
+
+func (l *LeveledLoggerAdapter) Debug(msg string, keysAndValues ...any) {
+	l.log.Debug(msg, keysAndValues...)
+}
+
+func (l *LeveledLoggerAdapter) Info(msg string, keysAndValues ...any) {
+	l.log.Info(msg, keysAndValues...)
+}
+
+func (l *LeveledLoggerAdapter) Warn(msg string, keysAndValues ...any) {
+	l.log.Info(msg, keysAndValues...)
+}
+
+func (l *LeveledLoggerAdapter) Error(msg string, keysAndValues ...any) {
+	l.log.Info(msg, keysAndValues...)
+}
+
+func (l *LeveledLoggerAdapter) Panic(msg string, keysAndValues ...any) {
+	l.log.Panic(msg, keysAndValues...)
+}
