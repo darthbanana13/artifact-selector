37b716b933fb6f8809e4fc8aad358d692d2d74d3
Radu Margineanu <radu.margineanu@gmail.com>
2025-07-14 16:07:17 +0200

    Clean up Github package

--- pkg/github/github.go
+++ pkg/github/github.go
@@ -5,7 +5,6 @@ import (
   "fmt"
   "io"
   "net/http"
-  "strings"
 )
 
 const LatestReleases = "https://api.github.com/repos/%s/releases/latest"
@@ -20,22 +19,21 @@ type ReleasesInfo struct {
 type Artifact struct {
   InfoUrl       string  `json:"url"`
   FileName      string  `json:"name"`
-  FileType      string  `json:"content_type"`
+  ContentType   string  `json:"content_type"`
   Size          uint64  `json:"size"`
   DownloadLink  string  `json:"browser_download_url"`
 }
 
 type IFetcher interface {
   FetchArtifacts(string) (ReleasesInfo, error)
-  TestValidRepoName(string) error
-  MakeUrl(string, string) string
+  MakeUrl(string, string) (string, error)
   PrepareRequest(string) *http.Request
-  GetUrlBody(*http.Request, BodyReader) ([]byte, error)
-  ReadBody(io.Reader) ([]byte, error)
+  DoGetRequest(*http.Request) (*http.Response, error)
+  ReadResponseBody(io.Reader) ([]byte, error)
   ParseJson([]byte) (ReleasesInfo, error)
 }
 
-type httpClient interface {
+type HttpClient interface {
   Do(*http.Request) (*http.Response, error)
 }
 
@@ -45,10 +43,11 @@ type FetcherTemplate struct {
 
 type HttpFetcher struct {
   *FetcherTemplate
-  c httpClient 
+  c HttpClient 
+  t string
 }
 
-func NewHttpFetcher(client httpClient) *HttpFetcher {
+func NewHttpFetcher(client HttpClient) *HttpFetcher {
   f := &HttpFetcher{ 
     c:  client,
   }
@@ -57,17 +56,23 @@ func NewHttpFetcher(client httpClient) *HttpFetcher {
   return f
 }
 
-type BodyReader func(io.Reader) ([]byte, error)
+func NewDefaultHttpFetcher() *HttpFetcher {
+  return NewHttpFetcher(http.DefaultClient)
+}
 
 func (f *FetcherTemplate) FetchArtifacts(userRepo string) (ReleasesInfo, error) {
-  if err := f.TestValidRepoName(userRepo); err != nil {
+  repoUrl, err := f.MakeUrl(userRepo, LatestReleases)
+  if err != nil {
     return ReleasesInfo{}, err
   }
 
-  body, err := f.GetUrlBody(
-    f.PrepareRequest(f.MakeUrl(userRepo, LatestReleases)),
-    f.ReadBody,
-  )
+  resp, err := f.DoGetRequest(f.PrepareRequest(repoUrl))
+  defer resp.Body.Close()
+  if err != nil {
+    return ReleasesInfo{}, err
+  }
+
+  body, err := f.ReadResponseBody(resp.Body)
   if err != nil {
     return ReleasesInfo{}, err
   }
@@ -75,57 +80,35 @@ func (f *FetcherTemplate) FetchArtifacts(userRepo string) (ReleasesInfo, error) 
   return f.ParseJson(body)
 }
 
-func (HttpFetcher) TestValidRepoName(userRepo string) error {
-  i := strings.Index(userRepo, "/")
-  if i > 0 && i < len(userRepo) - 1 && strings.Count(userRepo, "/") == 1 {
-    return nil 
-  }
-  return fmt.Errorf("Given repo '%v', does not have the format 'user/repo-name'", userRepo)
-}
-
-func (HttpFetcher) MakeUrl(userRepo string, releasesUrlTemplate string) string {
-  return fmt.Sprintf(releasesUrlTemplate, userRepo)
+func (HttpFetcher) MakeUrl(userRepo string, releasesUrlTemplate string) (string, error) {
+  return fmt.Sprintf(releasesUrlTemplate, userRepo), nil
 }
 
 func (f *HttpFetcher) PrepareRequest(url string) *http.Request {
-  // https://docs.github.com/en/rest/releases/releases?apiVersion=2022-11-28#get-the-latest-release
   req, err := http.NewRequest(http.MethodGet, url, nil)
   if err != nil {
-    panic("The standard library for http has changed")
+    panic("The standard library for net/http has changed")
   }
+  // https://docs.github.com/en/rest/releases/releases?apiVersion=2022-11-28#get-the-latest-release
   req.Header.Set("Accept", "application/vnd.github+json")
   req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
-  // req.Header.Set("Authorization", "Bearer <TOKEN>")
+  // if len(f.t) > 0 {
+  //   req.Header.Set("Authorization", "Bearer " + f.t)
+  // }
 
   return req
 }
 
-func (f *HttpFetcher) GetUrlBody(req *http.Request, reader BodyReader) ([]byte, error) {
-  resp, err := f.c.Do(req)
-  if err != nil {
-    return nil, err
-  }
-  defer resp.Body.Close()
-
-  if resp.StatusCode != http.StatusOK {
-    return nil, fmt.Errorf("Expected HTTP Response Code %v, got code %v instead.", http.StatusOK, resp.StatusCode)
-  }
-
-  return reader(resp.Body)
+func (f *HttpFetcher) DoGetRequest(req *http.Request) (*http.Response, error) {
+  return f.c.Do(req)
 }
 
-func (HttpFetcher) ReadBody(r io.Reader) ([]byte, error) {
-  body, err := io.ReadAll(r)
-  if err != nil {
-    return body, err
-  }
-  return body, nil
+func (HttpFetcher) ReadResponseBody(r io.Reader) ([]byte, error) {
+  return io.ReadAll(r)
 }
 
 func (HttpFetcher) ParseJson(body []byte) (ReleasesInfo, error) {
   var info ReleasesInfo
-  if err := json.Unmarshal(body, &info); err != nil {
-    return info, fmt.Errorf("Could not parse Github API reponse. Has the API changed?")
-  }
-  return info, nil
+  err := json.Unmarshal(body, &info)
+  return info, err
 }
